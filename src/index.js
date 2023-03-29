bf32a00eafb3fb1afd83c411355e84468734e9cb
Qqkyu <kulmisiu007@gmail.com>
2023-03-29 18:29:03 +0200

    Move connection.js and index.js file to src directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { routes } from "./src/routes/index.js";
+import { routes } from "./routes/index.js";
 
 dotenv.config();
 
