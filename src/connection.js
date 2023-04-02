0662e6758a126f97ebdaf12199f7e61ef3e3c1a1
Qqkyu <kulmisiu007@gmail.com>
2023-04-02 17:34:39 +0200

    Add taboo cards cache

--- src/connection.js
+++ src/connection.js
@@ -2,13 +2,19 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
 dotenv.config();
+const client = new MongoClient(process.env.ATLAS_URI);
+
+const TABOO_CARDS_CACHE = undefined;
 
 export async function getTabooCards() {
-  const client = new MongoClient(process.env.ATLAS_URI);
+  if (TABOO_CARDS_CACHE) {
+    return TABOO_CARDS_CACHE;
+  }
 
   try {
     const database = client.db("taboo_cards");
     const cards = await database.collection("cards").find({}).toArray();
+    TABOO_CARDS_CACHE = cards;
     return cards;
   } finally {
     // Ensures that the client will close on finish/error
