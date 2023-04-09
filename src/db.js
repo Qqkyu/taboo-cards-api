9e7b560237133a8e71e2b10fd5c8969a323e9e02
Qqkyu <kulmisiu007@gmail.com>
2023-04-09 12:16:48 +0200

    Extract database name

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import { DATABASE_NAME } from "./constants/db.constants";
 
 dotenv.config();
 const client = new MongoClient(process.env.ATLAS_URI);
-export const database = client.db("taboo_cards");
+export const database = client.db(DATABASE_NAME);
