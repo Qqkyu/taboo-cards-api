cd1de22591b9be1b7e1b9bb9825ebbb3896e4e0a
Qqkyu <kulmisiu007@gmail.com>
2023-07-09 19:56:44 +0200

    Add landing page

--- src/server.js
+++ src/server.js
@@ -4,13 +4,20 @@ import cardRouter from "#resources/card/card.router.js";
 import { rateLimiter } from "#middlewares/rateLimiter.middleware.js";
 import { cacheCheck } from "#middlewares/cache.middleware.js";
 import { languageCheck } from "#middlewares/language.middleware.js";
+import { fileURLToPath } from "url";
+import path from "path";
 import cors from "cors";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(rateLimiter);
 app.use(cacheCheck);
