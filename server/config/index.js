7ec814aae562b71a96759a7e4d59e019ab651c66
Qqkyu <kulmisiu007@gmail.com>
2023-07-15 11:23:25 +0200

    Add vscode tasks

--- server/config/index.js
+++ server/config/index.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || "development";
 const baseConfig = {
   env,
   isDev: env === "development",
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 7777,
 };
 
 export default baseConfig;
