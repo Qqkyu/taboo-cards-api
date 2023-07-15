bfdd1017dbf323bbc1c9c1cc2add4a232b23dfd7
Qqkyu <kulmisiu007@gmail.com>
2023-07-15 15:28:42 +0200

    Add daisyUI

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,9 @@
 import { FunctionComponent } from "react";
 
 export const Header: FunctionComponent = () => {
-  return <div className="bg-purple-500">Header</div>;
+  return (
+    <div className="navbar bg-base-100">
+      <button className="btn btn-ghost normal-case text-xl">Taboo cards API</button>
+    </div>
+  );
 };
