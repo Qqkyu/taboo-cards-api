488f71c7389d5e0fe31fce937dc64232233e3fe9
Qqkyu <kulmisiu007@gmail.com>
2023-07-15 12:13:59 +0200

    Setup frontend eslint

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent } from "react";
 
-export const Header : FunctionComponent = () => {
+export const Header: FunctionComponent = () => {
   return <div className="bg-purple-500">Header</div>;
 };
