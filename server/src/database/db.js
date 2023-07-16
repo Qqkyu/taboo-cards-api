ca670b306297253c64c843cdf5cdde66476a5eb2
Qqkyu <kulmisiu007@gmail.com>
2023-07-16 16:44:52 +0200

    Move .env file

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -2,6 +2,6 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { DATABASE_NAME } from "#constants/db.constants.js";
 
-dotenv.config();
+dotenv.config({ path: "../.env" });
 const client = new MongoClient(process.env.ATLAS_URI);
 export const database = client.db(DATABASE_NAME);
