f9c79f2c93dc917765b2c91d601431d81f807aa8
Qqkyu <kulmisiu007@gmail.com>
2023-07-17 20:19:32 +0200

    Improve types and css

--- frontend/src/components/cards-carousel/cards-carousel.component.tsx
+++ frontend/src/components/cards-carousel/cards-carousel.component.tsx
@@ -1,8 +1,11 @@
 import { FunctionComponent } from "react";
+import { Card } from "src/types/card.types";
+
+const CARDS_IN_CAROUSEL = 10;
 
 const response = await fetch("http://localhost:7777/api/cards");
 const data = await response.json();
-const cards = data.data;
+const cards: Array<Card> = data.data;
 
 const BADGE_COLOR = {
   easy: "badge-success",
@@ -12,28 +15,28 @@ const BADGE_COLOR = {
 
 export const CardsCarousel: FunctionComponent = () => {
   return (
-    <div className="carousel w-72">
-      {cards.slice(0, 10).map((card, i) => {
+    <div className="carousel w-80">
+      {cards.slice(0, CARDS_IN_CAROUSEL).map(({ title, forbiddenWords, difficulty }, i) => {
         return (
-          <div id={`slide${i}`} className="carousel-item relative w-72">
-            <div className="card w-72 shadow-xl bg-primary">
+          <div key={title} id={`slide${i}`} className="carousel-item relative w-80 m-5">
+            <div className="card w-80 shadow-xl bg-primary">
               <div className="card-body flex flex-col items-center gap-5">
-                <h2 className="card-title">
-                  {card.title}
-                  <div className={`badge ${BADGE_COLOR[card.difficulty]}`}>{card.difficulty}</div>
+                <h2 className="card-title flex justify-between">
+                  {title}
+                  <div className={`badge ${BADGE_COLOR[difficulty]}`}>{difficulty}</div>
                 </h2>
                 <div className="flex flex-col items-center gap-4">
-                  {card.forbiddenWords.map((word) => (
+                  {forbiddenWords.map((word) => (
                     <p>{word}</p>
                   ))}
                 </div>
               </div>
             </div>
             <div className="absolute flex justify-between -translate-y-1/2 inset-x-2 top-1/2">
-              <a href={`#slide${i === 0 ? 9 : i - 1}`} className="btn btn-outline btn-circle">
+              <a href={`#slide${i === 0 ? CARDS_IN_CAROUSEL - 1 : i - 1}`} className="btn btn-outline btn-circle">
                 ❮
               </a>
-              <a href={`#slide${i === 9 ? 0 : i + 1}`} className="btn btn-outline btn-circle">
+              <a href={`#slide${i === CARDS_IN_CAROUSEL - 1 ? 0 : i + 1}`} className="btn btn-outline btn-circle">
                 ❯
               </a>
             </div>
---
+++ frontend/src/types/card.types.ts
@@ -0,0 +1,5 @@
+export type Card = {
+  title: string;
+  forbiddenWords: Array<string>;
+  difficulty: "easy" | "medium" | "hard";
+};
