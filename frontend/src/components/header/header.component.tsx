feebdb5873c9232c94e3f5af62eafc5d4c55546b
Qqkyu <kulmisiu007@gmail.com>
2023-07-20 22:40:14 +0200

    Add repo link with github icon

--- frontend/src/components/header/header.component.tsx
+++ frontend/src/components/header/header.component.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import { ThemeToggle } from "./_components/theme-toggle/theme-toggle.component";
+import { Icon } from "@/components/icon/icon.component";
 
 export const Header: FunctionComponent = () => {
   return (
@@ -9,9 +10,12 @@ export const Header: FunctionComponent = () => {
           Taboo cards API
         </a>
       </div>
-      <div className="navbar-end hidden md:flex gap-3">
-        <a href="#card-model" className="btn-ghost btn-outline btn text-xl normal-case text-primary-content">
+      <div className="navbar-end hidden md:flex gap-4">
+        <a href="#card-model" className="btn-ghost btn-outline btn text-lg normal-case text-primary-content">
           <span>Card model</span>
+        </a>
+        <a href="https://github.com/Qqkyu/taboo-cards-api" className="btn btn-circle btn-sm normal-case">
+          <Icon type="github" color="hsl(var(--pc))" />
         </a>
         <ThemeToggle />
       </div>
--- frontend/src/components/icon/icon.component.tsx
+++ frontend/src/components/icon/icon.component.tsx
@@ -2,8 +2,9 @@ import { FunctionComponent } from "react";
 import { MoonIcon } from "./icons/moon.icon";
 import { IconComponentProps } from "./icons/icon-props";
 import { SunIcon } from "./icons/sun.icon";
+import { GithubIcon } from "./icons/github.icon";
 
-export type IconType = "moon" | "sun";
+export type IconType = "moon" | "sun" | "github";
 
 type IconProps = IconComponentProps & {
   type: IconType;
@@ -12,6 +13,7 @@ type IconProps = IconComponentProps & {
 const Icons: Record<IconType, FunctionComponent<IconComponentProps>> = {
   moon: MoonIcon,
   sun: SunIcon,
+  github: GithubIcon,
 };
 
 export const Icon: FunctionComponent<IconProps> = ({ type, ...rest }) => {
