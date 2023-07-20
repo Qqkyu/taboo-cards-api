951f25570b78449802fb30151547271601e03c7c
Qqkyu <kulmisiu007@gmail.com>
2023-07-20 20:23:16 +0200

    Add @tailwindcss/typography plugin

--- frontend/src/components/section/section.component.tsx
+++ frontend/src/components/section/section.component.tsx
@@ -6,8 +6,8 @@ type Props = PropsWithChildren<{
 
 export const Section: FunctionComponent<Props> = ({ header, children }) => {
   return (
-    <section className="flex flex-col gap-5">
-      <h2>{header}</h2>
+    <section className="flex flex-col gap-5 p-5">
+      <h2 className="prose prose-2xl">{header}</h2>
       {children}
     </section>
   );
