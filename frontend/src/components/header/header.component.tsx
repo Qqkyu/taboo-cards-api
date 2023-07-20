de42da7ec5c560235fc35f4325196acdd05ff3ef
Qqkyu <kulmisiu007@gmail.com>
2023-07-20 20:37:36 +0200

    Add card model link in navbar

--- frontend/src/components/header/header.component.tsx
+++ frontend/src/components/header/header.component.tsx
@@ -1,17 +1,16 @@
 import { FunctionComponent } from "react";
-import { Icon } from "@/components/icon/icon.component";
 
 export const Header: FunctionComponent = () => {
   return (
-    <div className="navbar bg-base-100 px-5">
+    <div className="navbar bg-base-100 p-4">
       <div className="navbar-start">
-        <div className="dropdown">
-          <button tabIndex={0} className="btn btn-ghost md:hidden">
-            <Icon type="burger-menu" color="hsl(var(--pc))" className="h-5 w-5" />
-          </button>
-        </div>
         <a href="/" className="btn-ghost btn text-xl normal-case text-primary-content">
           Taboo cards API
+        </a>
+      </div>
+      <div className="navbar-end hidden md:flex gap-3">
+        <a href="#card-model" className="btn-ghost btn-outline btn text-xl normal-case text-primary-content">
+          <span>Card model</span>
         </a>
       </div>
     </div>
