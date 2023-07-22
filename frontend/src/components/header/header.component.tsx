02aabb5ef394e5aa8e6a221da46ed708d8178f50
Qqkyu <kulmisiu007@gmail.com>
2023-07-22 12:39:51 +0200

    Make header fixed

--- frontend/src/components/header/header.component.tsx
+++ frontend/src/components/header/header.component.tsx
@@ -4,13 +4,16 @@ import { Icon } from "@/components/icon/icon.component";
 
 export const Header: FunctionComponent = () => {
   return (
-    <div className="navbar bg-base-300 p-4">
+    <div className="navbar z-10 fixed bg-base-300 p-4">
       <div className="navbar-start">
         <a href="/" className="btn-ghost btn text-xl normal-case text-primary-content">
           Taboo cards API
         </a>
       </div>
       <div className="navbar-end flex gap-4">
+        <a href="#cards" className="btn-ghost btn-outline btn text-lg normal-case text-primary-content">
+          <span>Cards</span>
+        </a>
         <a href="#docs" className="btn-ghost btn-outline btn text-lg normal-case text-primary-content">
           <span>Docs</span>
         </a>
