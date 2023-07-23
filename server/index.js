87f5950c9d9a62c80f9a54c7fb1711217de5e89a
Qqkyu <kulmisiu007@gmail.com>
2023-07-23 17:25:30 +0200

    Hook astro built with express backend

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.use(rateLimiter);
 app.use(cacheCheck);
@@ -27,7 +27,7 @@ app.use("/", (req, res, next) => next());
 app.use("/api/cards", cardRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 try {
