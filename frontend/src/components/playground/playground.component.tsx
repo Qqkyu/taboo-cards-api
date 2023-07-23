ffd87f833fdde9a9ac276fe67e53b1514d10900a
Qqkyu <kulmisiu007@gmail.com>
2023-07-23 19:09:00 +0200

    Handle enter key

--- frontend/src/components/playground/playground.component.tsx
+++ frontend/src/components/playground/playground.component.tsx
@@ -1,8 +1,7 @@
-import { FunctionComponent, useCallback, useState } from "react";
+import { FunctionComponent, KeyboardEvent, useCallback, useState } from "react";
 import ReactJson from "react-json-view";
 
-// const API_URL_PREFIX = `${window.location}api/`;
-const API_URL_PREFIX = `http://localhost:7777/api/`;
+const API_URL_PREFIX = `${window.location}api/`;
 
 export const Playground: FunctionComponent = () => {
   const [value, setValue] = useState("cards");
@@ -18,6 +17,12 @@ export const Playground: FunctionComponent = () => {
     }
   }, [value]);
 
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleClick();
+    }
+  };
+
   return (
     <div id="playground" className="flex flex-col gap-8">
       <h2 className="prose prose-2xl">Playground</h2>
@@ -26,10 +31,11 @@ export const Playground: FunctionComponent = () => {
         <input
           type="text"
           onChange={(e) => setValue(e.target.value)}
+          onKeyUp={handleKeyUp}
           value={value}
           className="input join-item input-bordered"
         />
-        <button onClick={handleClick} className="btn join-item">
+        <button onClick={handleClick} className="btn bg-base-300 join-item">
           Request
         </button>
       </div>
