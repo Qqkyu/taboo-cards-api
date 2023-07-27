a63dd42c49269fe2638084b9c8275b37b5853f3a
Qqkyu <kulmisiu007@gmail.com>
2023-07-27 20:33:31 +0200

    Fix path

--- frontend/src/paths/api.paths.ts
+++ frontend/src/paths/api.paths.ts
@@ -3,7 +3,7 @@ import process from "process";
 const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 export const API_URL_PREFIX = IS_DEVELOPMENT
   ? "http://localhost:7777/api/"
-  : "https://taboocardsapi.com/api/";
+  : "https://www.taboocardsapi.com/api/";
 
 export const CARDS_PATHS = {
   cards: `${API_URL_PREFIX}cards`,
