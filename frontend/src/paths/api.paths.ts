a56d3b344903da4ff7fc5cc014a18a91430af480
Qqkyu <kulmisiu007@gmail.com>
2023-07-27 21:12:33 +0200

    Use 10000 port both in development and prod

--- frontend/src/paths/api.paths.ts
+++ frontend/src/paths/api.paths.ts
@@ -1,9 +1,6 @@
 import process from "process";
 
-const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-export const API_URL_PREFIX = IS_DEVELOPMENT
-  ? "http://localhost:7777/api/"
-  : "https://www.taboocardsapi.com/api/";
+export const API_URL_PREFIX = "http://localhost:10000/api/";
 
 export const CARDS_PATHS = {
   cards: `${API_URL_PREFIX}cards`,
