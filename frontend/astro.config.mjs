ab4b352c8be359776054b402af48d1d15500ec7d
Qqkyu <kulmisiu007@gmail.com>
2023-07-28 20:15:49 +0200

    Remove netlify files

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [netlify(), react(), tailwind()],
-  output: "server",
+  integrations: [react(), tailwind()],
   outDir: "../dist",
 });
