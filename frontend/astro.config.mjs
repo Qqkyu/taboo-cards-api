118ef1714bb49dcaf9f39c771ab1463811bfe240
Qqkyu <kulmisiu007@gmail.com>
2023-07-28 19:01:40 +0200

    Add astro netlify adapter

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [netlify(), react(), tailwind()],
+  output: "server",
   outDir: "../dist",
 });
