dd33054d4725d2d4bab8e2974aa2b2a2b29f8fd0
Qqkyu <kulmisiu007@gmail.com>
2023-07-29 13:11:18 +0200

    Swap dev and prod paths

--- frontend/src/paths/api.paths.ts
+++ frontend/src/paths/api.paths.ts
@@ -3,7 +3,7 @@ import process from "process";
 const IS_DEVELOPMENT = !process.env.NODE_ENV || process.env.NODE_ENV;
 
 export const LOCALHOST_API_URL_PREFIX = "http://localhost:10000/api/";
-export const WEB_API_URL_PREFIX = IS_DEVELOPMENT ? LOCALHOST_API_URL_PREFIX : "http://taboocardsapi.com/api/";
+export const WEB_API_URL_PREFIX = IS_DEVELOPMENT ? "https://taboocardsapi.com/api/" : LOCALHOST_API_URL_PREFIX;
 
 export const CARDS_PATHS = {
   localhost: {
