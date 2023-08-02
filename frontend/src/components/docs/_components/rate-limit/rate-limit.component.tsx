6a036e5b3c6a947dad5eb06f11f0f91e648d350d
Qqkyu <kulmisiu007@gmail.com>
2023-08-02 19:03:35 +0200

    Fix rate-limit doc

--- frontend/src/components/docs/_components/rate-limit/rate-limit.component.tsx
+++ frontend/src/components/docs/_components/rate-limit/rate-limit.component.tsx
@@ -5,7 +5,7 @@ export const RateLimit: FunctionComponent = () => {
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-6">
         <p className="prose prose-sm lg:prose-base">
-          There is a 100 requests per day API limit to prevent any malicious action. If you were to make more requests
+          There is a 10000 requests per day API limit to prevent any malicious action. If you were to make more requests
           than this, you will get error response with 429 status response (Too Many Requests). This will likely be
           increased in the near future.
         </p>
