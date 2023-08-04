f01ad95fc48d6b687d793c09084345f786ae2ed2
Qqkyu <kulmisiu007@gmail.com>
2023-08-04 20:53:26 +0200

    Fix docs sections width

--- frontend/src/components/docs/_components/endpoints/endpoints.component.tsx
+++ frontend/src/components/docs/_components/endpoints/endpoints.component.tsx
@@ -4,7 +4,7 @@ export const Endpoints: FunctionComponent = () => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-6">
-        <p className="prose prose-sm lg:prose-base">
+        <p className="prose prose-sm lg:prose-base max-w-full">
           There are currently two endpoints. First endpoint returns all of the cards stored in the database. The
           response is the array of card objects.
         </p>
@@ -13,7 +13,7 @@ export const Endpoints: FunctionComponent = () => {
             <code>/api/cards</code>
           </pre>
         </div>
-        <p className="prose prose-sm lg:prose-base">
+        <p className="prose prose-sm lg:prose-base max-w-full">
           Second one randomly chooses one card and returns it. The response is a single card object:
         </p>
         <div className="mockup-code">
@@ -24,14 +24,14 @@ export const Endpoints: FunctionComponent = () => {
       </div>
       <div className="flex flex-col gap-6">
         <h3 className="prose prose-lg lg:prose-xl font-medium">Query parameters</h3>
-        <p className="prose prose-sm lg:prose-base">
+        <p className="prose prose-sm lg:prose-base max-w-full">
           In both of the above endpoints, you can attach following language and difficulty query parameters.
         </p>
         <div className="flex flex-col gap-4">
-          <h4 className="prose prose-base lg:prose-lg">
+          <h4 className="prose prose-base lg:prose-lg max-w-full">
             <code>language</code> query parameter
           </h4>
-          <p className="prose prose-sm lg:prose-base">
+          <p className="prose prose-sm lg:prose-base max-w-full">
             Attach <code>language</code> query parameter to get cards only in the provided language (either{" "}
             <code>en</code>
             or <code>pl</code>):
@@ -44,7 +44,7 @@ export const Endpoints: FunctionComponent = () => {
               <code>/api/cards?language=pl</code>
             </pre>
           </div>
-          <p className="prose prose-sm lg:prose-base">
+          <p className="prose prose-sm lg:prose-base max-w-full">
             Not providing <code>language</code>
             query parameter is equal to providing <code>language=en</code>, so following endpoints are equal:
           </p>
@@ -56,7 +56,7 @@ export const Endpoints: FunctionComponent = () => {
               <code>/api/cards?language=en</code>
             </pre>
           </div>
-          <p className="prose prose-sm lg:prose-base">
+          <p className="prose prose-sm lg:prose-base max-w-full">
             In both of above cases array of cards in English will be returned.
           </p>
         </div>
@@ -64,7 +64,7 @@ export const Endpoints: FunctionComponent = () => {
           <h4 className="prose prose-base lg:prose-lg">
             <code>difficulty </code> query parameter
           </h4>
-          <p className="prose prose-sm lg:prose-base">
+          <p className="prose prose-sm lg:prose-base max-w-full">
             Attach <code>difficulty </code> query parameter to get cards only with the provided difficulty (
             <code>easy</code>, <code>medium</code>, or <code>hard</code>):
           </p>
@@ -79,18 +79,18 @@ export const Endpoints: FunctionComponent = () => {
               <code>/api/cards?difficulty=hard</code>
             </pre>
           </div>
-          <p className="prose prose-sm lg:prose-base">
+          <p className="prose prose-sm lg:prose-base max-w-full">
             When <code>difficulty</code> query parameter is not provided, the response will be:
           </p>
           <dl>
             <dt>
               <code>/api/cards</code>
             </dt>
-            <dd className="prose prose-sm lg:prose-base pl-4">All cards with mixed difficulties.</dd>
+            <dd className="prose prose-sm lg:prose-base max-w-full pl-4">All cards with mixed difficulties.</dd>
             <dt>
               <code>/api/cards/random</code>
             </dt>
-            <dd className="prose prose-sm lg:prose-base pl-4">One card with any difficulty.</dd>
+            <dd className="prose prose-sm lg:prose-base max-w-full pl-4">One card with any difficulty.</dd>
           </dl>
         </div>
       </div>
--- frontend/src/components/docs/_components/rate-limit/rate-limit.component.tsx
+++ frontend/src/components/docs/_components/rate-limit/rate-limit.component.tsx
@@ -4,7 +4,7 @@ export const RateLimit: FunctionComponent = () => {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-6">
-        <p className="prose prose-sm lg:prose-base">
+        <p className="prose prose-sm lg:prose-base max-w-full">
           There is a 10000 requests per day API limit to prevent any malicious action. If you were to make more requests
           than this, you will get error response with 429 status response (Too Many Requests). This will likely be
           increased in the near future.
