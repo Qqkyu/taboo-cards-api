15a5d3df2681a3af0b3322b20d444aa5120dbf48
Qqkyu <kulmisiu007@gmail.com>
2023-08-12 11:17:50 +0200

    Add docs dividersP

--- frontend/src/components/docs/docs.component.tsx
+++ frontend/src/components/docs/docs.component.tsx
@@ -7,16 +7,19 @@ import { BaseUrl } from "./_components/base-url/base-url.component";
 
 export const Docs: FunctionComponent = () => {
   return (
-    <div className="flex w-[300px] flex-col gap-14 sm:w-[500px] lg:w-[750px]">
+    <div className="flex w-[300px] flex-col sm:w-[500px] lg:w-[750px]">
       <Section id="card-model" header="Card model">
         <CardModel />
       </Section>
+      <div className="divider" />
       <Section id="endpoints" header="Endpoints">
         <Endpoints />
       </Section>
+      <div className="divider" />
       <Section id="base-url" header="Base URL">
         <BaseUrl />
       </Section>
+      <div className="divider" />
       <Section id="rate-limit" header="Rate limit">
         <RateLimit />
       </Section>
