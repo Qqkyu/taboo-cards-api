285e39283834ede07206db2e52f989efe5dc126a
Qqkyu <kulmisiu007@gmail.com>
2023-08-23 19:48:58 +0200

    Theme toggle fixes with react skeleton loader pt 1

--- frontend/src/components/header/_components/theme-toggle/theme-toggle.component.tsx
+++ frontend/src/components/header/_components/theme-toggle/theme-toggle.component.tsx
@@ -1,8 +1,20 @@
+import Skeleton from "react-loading-skeleton";
 import { FunctionComponent, useEffect, useMemo, useState } from "react";
 import { Icon } from "@/components/icon/icon.component";
+import "react-loading-skeleton/dist/skeleton.css";
 
 export const ThemeToggle: FunctionComponent = () => {
+  const [isMounted, setIsMounted] = useState(false);
   const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const initialTheme = useMemo(() => theme, []);
@@ -11,12 +23,7 @@ export const ThemeToggle: FunctionComponent = () => {
     theme === "pastel" ? setTheme("dark") : setTheme("pastel");
   };
 
-  useEffect(() => {
-    document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
-  }, [theme]);
-
-  return (
+  return isMounted ? (
     <label className="swap swap-rotate">
       <input type="checkbox" onClick={handleClick} />
 
@@ -32,5 +39,9 @@ export const ThemeToggle: FunctionComponent = () => {
         className={`w-6 sm:w-7 lg:w-8 ${initialTheme === "pastel" ? "swap-off" : "swap-on"}`}
       />
     </label>
+  ) : (
+    <div className="h-6 w-6 sm:h-7 sm:w-7 lg:h-8 lg:w-8">
+      <Skeleton circle height="100%" />
+    </div>
   );
 };
