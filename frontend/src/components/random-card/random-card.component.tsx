53d342b97f890bbd2b38bb7b3bb6ed9da231b62e
Qqkyu <kulmisiu007@gmail.com>
2023-09-10 14:20:13 +0200

    Basic 'Just cards' mode implementation

---
+++ frontend/src/components/random-card/random-card.component.tsx
@@ -0,0 +1,71 @@
+import { Font } from "@/design-system/font/font.component";
+import { getRandomCard } from "@/lib/api";
+import { Card } from "@/types/card.types";
+import { FunctionComponent, useCallback, useEffect, useRef, useState } from "react";
+
+const BADGE_COLOR = {
+  easy: "badge-success",
+  medium: "badge-warning",
+  hard: "badge-error",
+} as const;
+
+type Props = {
+  lang: "en" | "pl";
+};
+
+export const RandomCard: FunctionComponent<Props> = ({ lang }) => {
+  const previousCards = useRef<Array<Card>>([]);
+  const [card, setCard] = useState<Card | undefined>(undefined);
+
+  const setRandomCard = useCallback(async () => {
+    const randomCard = await getRandomCard({ lang });
+    setCard(randomCard);
+  }, [lang]);
+
+  useEffect(() => {
+    setRandomCard();
+  }, [lang, setRandomCard]);
+
+  const handleNextCardClick = useCallback(() => {
+    previousCards.current.push(card);
+    setRandomCard();
+  }, [card, setRandomCard]);
+
+  const handlePrevCardClick = useCallback(() => {
+    const previousCard = previousCards.current.pop();
+    previousCard && setCard(previousCard);
+  }, []);
+
+  return (
+    card && (
+      <div className="flex flex-col gap-4">
+        <div className="card bg-primary w-80 shadow-xl">
+          <div className="card-body flex flex-col items-center gap-3 sm:gap-6 lg:gap-8">
+            <div className="card-title text-center">
+              <Font.H2 color="text-primary-content">{card.title}</Font.H2>
+            </div>
+            <div className="flex flex-col items-center gap-3">
+              {card.forbiddenWords.map((word) => (
+                <Font.P2 key={word} color="text-primary-content">
+                  {word}
+                </Font.P2>
+              ))}
+            </div>
+            <div className={`badge h-5 ${BADGE_COLOR[card.difficulty]}`}>{card.difficulty}</div>
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <button
+            className={`btn btn-outline btn-secondary ${previousCards.current.length === 0 ? "btn-disabled" : ""}`}
+            onClick={handlePrevCardClick}
+          >
+            ❮ Previous
+          </button>
+          <button className="btn btn-secondary" onClick={handleNextCardClick}>
+            Next ❯
+          </button>
+        </div>
+      </div>
+    )
+  );
+};
